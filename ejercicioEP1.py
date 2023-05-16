76ab4f4da268529418fda8aaa192f00d2b5d98ed
Ale Santos Coz <ale.santoscoz@gmail.com>
2023-05-15 21:21:41 -0500

    corrección en clase

--- ejercicioEP1.py
+++ ejercicioEP1.py
@@ -18,28 +18,3 @@ frec_df.columns = ["fi"]
 #Creamos la columna de frec. rlativa yrealizamos las operaciones numéricas para calcularla
 frec_df["hi %"] = 100*frec_df["fi"]/len(db)
 
-#Obtenemos las frecuencias acumuladas
-#guardamos los valores en una variable
-fi_val = frec_df["fi"].values
-
-#creamos array vacío para acumular
-acum = []
-acum_val = 0 #le damos un valo inicial de 0
-
-#creamos un ciclo for para sumar los valores y acumularlos
-for i in fi_val:
-    acum_val = acum_val + i
-    acum.append(acum_val)
-
-#Creamos la columna Fi con los valores acumulados
-frec_df["Fi"] = acum
-
-#Lo mismo aplicamos para acumular frec. relativas
-acum_hi = []
-acumhi_val = 0 #le damos un valo inicial de 0
-hi_val = frec_df["hi %"].values
-for i in hi_val:
-    acumhi_val = acumhi_val + i
-    acum_hi.append(acumhi_val)
-
-frec_df["Hi %"] = acum_hi
